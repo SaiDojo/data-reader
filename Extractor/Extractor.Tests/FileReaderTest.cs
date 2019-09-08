7b01badaf1669c015e7897c11cc3ed5797698be1
SaiDojo <sairajreddy.k@gmail.com>
2019-09-08 10:26:57 -0500

    Added extract method-prints all the lines Added overloaded extract method takes in streamreader as input

---
+++ Extractor/Extractor.Tests/FileReaderTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extractor.Tests
+{
+    [TestClass]
+    public class FileReaderTest
+    {
+        [TestMethod]
+        public void TestReader()
+        {
+            var extractor = new Extractor();
+            int totalLines = extractor.Extract(@"C:\Users\saira_000\Desktop\Sai\dotnet\family.csv");
+            Assert.AreEqual(4, totalLines);
+        }
+    }
+}
---
+++ Extractor/Extractor/Extractor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extractor
+{
+    public interface IExtractor
+    {
+        int Extract(string filePath);
+        int Extract(StreamReader reader);
+    }
+
+    public class Extractor: IExtractor
+    {
+        public int Extract(StreamReader reader)
+        {
+            string line;
+            int totalLines = 0 ;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                totalLines++;
+            }
+            return totalLines;
+        }
+
+        public int Extract(string filepath)
+        {
+            using (var reader = new StreamReader(filepath))
+            {
+                return Extract(reader);
+            }
+        }
+
+    }
+}
